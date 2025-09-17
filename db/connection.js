f2064ca026f248380dbb9c9c6c87722f57ee82ce
David Chavarría <d.chavarria@alumnos.upm.es>
2025-09-17 18:16:17 +0200

    meter campos de base de datos

--- db/connection.js
+++ db/connection.js
@@ -1,23 +1,31 @@
 const mysql = require('mysql2/promise');
+const express = require('express');
 
+const app = express();
+const PORT = 3000;
+
+// Middleware para recibir JSON
+app.use(express.json());
+
+// Conexión a la base de datos
 async function conectarMySQL() {
   try {
     const connection = await mysql.createConnection({
-      host: 'localhost',     
-      port: 3306,            
-      user: 'root',           
-      password: 'root', 
-      database: 'taquillas'     
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'root',
+      database: 'taquillas'
     });
 
     console.log('✅ Conectado a MySQL');
-    const [rows] = await connection.execute('SELECT * FROM users');
-    console.log(rows);
-
-    await connection.end();
+    return connection;
   } catch (err) {
     console.error('❌ Error al conectar a MySQL:', err.message);
+    throw err;
   }
 }
 
-conectarMySQL();
+module.exports = {
+conectarMySQL
+}
--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const path = require('path');
 const app = express();
 const port = 2000;
 const mainRoutes = require('./routes/mainRoutes');
+const { conectarMySQL } = require('./db/connection')
 
 require('./db/connection');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('views'));
 
@@ -12,4 +16,24 @@ app.use('/', mainRoutes);
 
 app.listen(port, () => {
   console.log('Servidor escuchando en http://localhost:'+ port);
+});
+
+app.post('/guardar', async (req, res) => {
+  const { nombre, delega, DNI, ntaquilla, opciones } = req.body;
+
+  try {
+    const connection = await conectarMySQL();
+
+    await connection.execute(
+      'INSERT INTO users (nombre, delega , DNI, ntaquilla, opciones) VALUES (?, ?, ?, ?, ?)',
+      [nombre, delega, DNI, ntaquilla, opciones]
+    );
+
+    await connection.end();
+
+    res.send('✅ Datos insertados en la base de datos');
+  } catch (error) {
+    console.error('❌ Error al insertar en MySQL:', error.message);
+    res.status(500).send('Error al insertar en la base de datos');
+  }
 });
